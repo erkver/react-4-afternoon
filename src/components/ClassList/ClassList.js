1a468868f9985388214dc4a96a2fe75aed5bcaba
Eric Vere <erkver250@gmail.com>
2018-09-18 15:11:22 -0500

    Completed react-4-afternoon

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
-    
+  constructor(props) {
+    super(props)
+      this.state= {
+        students: []
+      }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then(res => {
+      console.log("res: ", res);
+      this.setState({students: res.data})
+    }).catch(err => console.log(err))
   }
 
   render() {
+    const { students } = this.state;
+    let studentList = students.map((e, ind) => (
+      <Link
+        to={`/student/${e.id}`}
+        key={ind}
+      >
+        <h3>
+          {e.first_name} {e.last_name}
+        </h3>
+      </Link>
+    ));
     return (
       <div className="box">
-        <h1></h1>
+        <button onClick={() => this.props.history.goBack()}>Back home page</button>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        { studentList }
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Student extends Component {
-  constructor() {
-    super()
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      studentInfo: {}
+    }
   }
 
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then(res => {
+      console.log("res :", res)
+      this.setState({studentInfo: res.data})
+    }).catch(err => console.log(err));
+  }
   render() {
+    console.log(this.props);
+    const { studentInfo } = this.state;
     return (
       <div className="box">
+        <button onClick={() => this.props.history.goBack()}>Back to class list</button>
         <h1>Student</h1>
+        <h1>{studentInfo.first_name}</h1>
+        <h1>{studentInfo.last_name}</h1>
+        <h3>Grade: {studentInfo.grade}</h3>
+        <h3>Email: {studentInfo.email}</h3>
       </div>
     )
   }
